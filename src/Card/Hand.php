7be89c887942be64202c7a16d744f30ab845c812
Alf Pettersson <alpt22@student.bth.se>
2024-04-19 23:43:03 +0200

    Add inheritence from Deck to DeckJoker

--- src/Card/Hand.php
+++ src/Card/Hand.php
@@ -12,39 +12,4 @@ class Hand extends Deck
     public function __construct() {
         $this->hand = $this->deck->draw(5);
     }
-
-    // public function add(Dice $die): void
-    // {
-    //     $this->hand[] = $die;
-    // }
-
-    // public function roll(): void
-    // {
-    //     foreach ($this->hand as $die) {
-    //         $die->roll();
-    //     }
-    // }
-
-    // public function getNumberDices(): int
-    // {
-    //     return count($this->hand);
-    // }
-
-    // public function getValues(): array
-    // {
-    //     $values = [];
-    //     foreach ($this->hand as $die) {
-    //         $values[] = $die->getValue();
-    //     }
-    //     return $values;
-    // }
-
-    // public function getString(): array
-    // {
-    //     $values = [];
-    //     foreach ($this->hand as $die) {
-    //         $values[] = $die->getAsString();
-    //     }
-    //     return $values;
-    // }
 }
